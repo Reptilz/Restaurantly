075d6b0d1623dd686a8160a3f4546f14a33ab51f
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-07-25 12:25:18 +0200

    push on github

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <NuxtLink to="restaurants" class="link"
+        >Go to restaurants <span>&#10140;</span></NuxtLink
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "Restaurantly - Welcome",
+});
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+.link {
+  text-decoration: none;
+  font-size: 1.5rem;
+}
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout name="custom">
+      <div class="container">
+        <RestaurantTable />
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "Restaurantly - Top 50",
+});
+</script>
+
+<style></style>
